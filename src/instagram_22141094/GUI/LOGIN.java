2f0c4a67937b4413bda2cd52e1284f724ed219b5
Debbido27 <davidmedina272004@gmail.com>
2026-03-02 22:01:51 -0600

    Creacion de metodo inico, y se pudo el gridbaglayout, para poder dividis los dos panales que usaremos para el logni

--- src/instagram_22141094/GUI/LOGIN.java
+++ src/instagram_22141094/GUI/LOGIN.java
@@ -1,7 +1,12 @@
 
 package instagram_22141094.GUI;
 
+import java.awt.CardLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class LOGIN  extends JFrame{
@@ -16,6 +21,16 @@ public class LOGIN  extends JFrame{
         setSize(1200,1200);
         setLocationRelativeTo(null);
         
+        CardLayout cd = new CardLayout();
+        JPanel inicar = new JPanel (cd);
+        
+    }
+    
+    private JPanel INICIO(){
+        JPanel inicio = new JPanel (new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets (0,0,0,0);
         
         
     }
