1be72db1337860011f5f902ac5cef3c62fb9ef3a
Debbido27 <davidmedina272004@gmail.com>
2026-03-02 21:58:14 -0600

    Se inicio el metodo de inciar, el cual sera el jframe padre de los demas metodos para la clase login

---
+++ src/instagram_22141094/GUI/LOGIN.java
@@ -0,0 +1,22 @@
+
+package instagram_22141094.GUI;
+
+import javax.swing.JFrame;
+
+
+public class LOGIN  extends JFrame{
+    
+    public LOGIN(){
+        
+    }
+    
+    private void INICIAR(){
+        setTitle("INSTAGRAM");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200,1200);
+        setLocationRelativeTo(null);
+        
+        
+        
+    }
+}
