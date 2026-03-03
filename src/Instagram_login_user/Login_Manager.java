49b985a624cca6148c628a746f0187442875b35a
Debbido27 <davidmedina272004@gmail.com>
2026-03-03 09:57:12 -0600

    Se creo la clase Login_Manager, y se crearon los atributos para poder manejar los users, de la clase user

--- src/Instagram_login_user/Login_Manager.java
+++ src/Instagram_login_user/Login_Manager.java
@@ -2,5 +2,18 @@
 package Instagram_login_user;
 
 public class Login_Manager {
+    private static final int MAX_USERS = 50;
+    
+    private User[] users;
+    private int totalUsers;
+    private User currentUser;
+    
+    public Login_Manager(){
+        users = new User[MAX_USERS];
+        totalUsers=0;
+        currentUser =null;
+        
+    }
+    
     
 }
