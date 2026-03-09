3735a8e2a1b0404fa1ff8ab8884e748fd2e4e4c3
Debbido27 <davidmedina272004@gmail.com>
2026-03-09 17:43:44 -0600

    Se crearon las variables y el constructor

--- src/instagram_22141094/GUI/PANEL_PERFIL_VISTA.java
+++ src/instagram_22141094/GUI/PANEL_PERFIL_VISTA.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package instagram_22141094.GUI;
 
-/**
- *
- * @author Dell
- */
-public class PANEL_PERFIL_VISTA {
+import Instagram_login_user.Login_Manager;
+import Instagram_login_user.User;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+
+public class PANEL_PERFIL_VISTA extends JPanel {
+    
+    private User userVisto;
+    private String usuarioLogueado;
+    private Login_Manager loginManager;
+    private CardLayout cardLayout;
+    private JPanel contentPanel;
+    
+    
+    public PANEL_PERFIL_VISTA(User userVisto, String usuarioLogueado, Login_Manager loginManager, CardLayout cardlayout, JPanel contentPanel){
+        this.userVisto=userVisto;
+        this.usuarioLogueado=usuarioLogueado;
+        this.loginManager=loginManager;
+        this.cardLayout=cardLayout;
+        this.contentPanel=contentPanel;
+        setLayout(new BorderLayout());
+        setBackground(new Color(18,18,18));
+        add(construir().BorderLayout.CENTER);
+        
+    }
     
 }
