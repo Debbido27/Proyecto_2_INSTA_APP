d52f575add353534742dcfe97e8ad86e54c95d97
Debbido27 <davidmedina272004@gmail.com>
2026-03-09 17:46:05 -0600

    Avances en construir

--- src/instagram_22141094/GUI/PANEL_PERFIL_VISTA.java
+++ src/instagram_22141094/GUI/PANEL_PERFIL_VISTA.java
@@ -6,6 +6,13 @@ import Instagram_login_user.User;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
@@ -26,7 +33,28 @@ public class PANEL_PERFIL_VISTA extends JPanel {
         this.contentPanel=contentPanel;
         setLayout(new BorderLayout());
         setBackground(new Color(18,18,18));
-        add(construir().BorderLayout.CENTER);
+        add(construir(), BorderLayout.CENTER);
+        
+    }
+    
+    private JPanel construir(){
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBackground(new Color(18,18,18));
+        
+               JPanel infoPanel = new JPanel(new GridBagLayout());
+        infoPanel.setBackground(new Color(18, 18, 18));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(40, 60, 20, 60));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 20, 5, 20);
+        gbc.anchor = GridBagConstraints.WEST;
+
+        // FOTO
+        JLabel fotoLabel = new JLabel();
+        fotoLabel.setPreferredSize(new Dimension(100, 100));
+        fotoLabel.setBackground(new Color(60, 60, 60));
+        fotoLabel.setOpaque(true);
+        fotoLabel.setFont(new Font("Arial", Font.BOLD,36));
         
     }
     
