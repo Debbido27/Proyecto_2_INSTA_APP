b1d9a91b7d12a0815056b63b527c39c7ab09bc74
Debbido27 <davidmedina272004@gmail.com>
2026-03-09 21:20:07 -0600

    Se termino el abrir perfil

--- src/instagram_22141094/GUI/PANEL_SEGUIDORES.java
+++ src/instagram_22141094/GUI/PANEL_SEGUIDORES.java
@@ -182,8 +182,20 @@ public class PANEL_SEGUIDORES extends JPanel {
     }
     
     private void abrirPerfil(User u ){
-        
+        for (int i = 0; i < contentPanel.getComponentCount(); i++) {
+            if ("PERFIL_BUSCADO".equals(contentPanel.getComponent(i).getName())) {
+                contentPanel.remove(i);
+                break;
+            }
+        }
+        PANEL_PERFIL_VISTA vista = new PANEL_PERFIL_VISTA(
+            u, usuarioLogueado, loginManager, followersManager, cardLayout, contentPanel
+        );
+        vista.setName("PERFIL_BUSCADO");
+        contentPanel.add(vista, "PERFIL_BUSCADO");
+        cardLayout.show(contentPanel, "PERFIL_BUSCADO");
+    }
     }
     
     
-}
+
