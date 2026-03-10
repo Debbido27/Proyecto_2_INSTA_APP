0f15e7a3beed80cea736c37609fdf72ceff77290
Debbido27 <davidmedina272004@gmail.com>
2026-03-09 21:10:25 -0600

    se crearon las variables y el constructor

--- src/instagram_22141094/GUI/PANEL_SEGUIDORES.java
+++ src/instagram_22141094/GUI/PANEL_SEGUIDORES.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package instagram_22141094.GUI;
 
-/**
- *
- * @author Dell
- */
-public class PANEL_SEGUIDORES {
+import Instagram_login_user.Followers_Manager;
+import Instagram_login_user.Login_Manager;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+
+public class PANEL_SEGUIDORES extends JPanel {
+    
+    private String usuarioLogueado;
+    private Login_Manager loginManager;
+    private Followers_Manager followersManager;
+    private CardLayout cardLayout;
+    private JPanel contentPanel;
+
+    public PANEL_SEGUIDORES(String usuarioLogueado, Login_Manager loginManager,
+                             Followers_Manager followersManager,
+                             CardLayout cardLayout, JPanel contentPanel) {
+        this.usuarioLogueado = usuarioLogueado;
+        this.loginManager = loginManager;
+        this.followersManager = followersManager;
+        this.cardLayout = cardLayout;
+        this.contentPanel = contentPanel;
+        setLayout(new BorderLayout());
+        setBackground(new Color(18, 18, 18));
+        construir();
+    }
+    
+    
     
 }
