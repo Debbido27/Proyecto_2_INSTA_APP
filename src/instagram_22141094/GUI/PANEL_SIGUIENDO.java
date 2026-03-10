4a4cb2afaf82d2ffcb7206f9a0c4beac379708ae
Debbido27 <davidmedina272004@gmail.com>
2026-03-09 21:22:39 -0600

    Se creo las variables y constructor

--- src/instagram_22141094/GUI/PANEL_SIGUIENDO.java
+++ src/instagram_22141094/GUI/PANEL_SIGUIENDO.java
@@ -1,7 +1,30 @@
 
 package instagram_22141094.GUI;
 
+import Instagram_login_user.Followers_Manager;
+import Instagram_login_user.Login_Manager;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
 
-public class PANEL_SIGUIENDO {
-    
+
+public class PANEL_SIGUIENDO extends JPanel {
+    private String usuarioLogueado;
+    private Login_Manager loginManager;
+    private Followers_Manager followersManager;
+    private CardLayout cardLayout;
+    private JPanel contentPanel;
+
+    public PANEL_SIGUIENDO(String usuarioLogueado, Login_Manager loginManager,
+                            Followers_Manager followersManager,
+                            CardLayout cardLayout, JPanel contentPanel) {
+        this.usuarioLogueado = usuarioLogueado;
+        this.loginManager = loginManager;
+        this.followersManager = followersManager;
+        this.cardLayout = cardLayout;
+        this.contentPanel = contentPanel;
+        setLayout(new BorderLayout());
+        setBackground(new Color(18, 18, 18));
+        construir();
+    }
 }
