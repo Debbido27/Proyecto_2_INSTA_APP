b7bcdf6053c5b9769397c0c1c06e2779153d7d1c
Debbido27 <davidmedina272004@gmail.com>
2026-03-10 21:14:15 -0600

    Creacion de variables

--- src/Instagram_login_user/Post.java
+++ src/Instagram_login_user/Post.java
@@ -2,5 +2,14 @@
 package Instagram_login_user;
 
 public class Post {
+ private String username;
+ private String contenido;
+ private String hashtags;
+ private String menciones;
+ private String rutaImagen;
+ private String tipoMedia;
+ private long fecha;
+ 
     
+ 
 }
