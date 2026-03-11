3e4850664b2e31a8ae55ba9c32a839025e2aaf63
Debbido27 <davidmedina272004@gmail.com>
2026-03-10 21:24:50 -0600

    Creacion de variables

--- src/Instagram_login_user/Post_Manager.java
+++ src/Instagram_login_user/Post_Manager.java
@@ -1,10 +1,20 @@
 
 package Instagram_login_user;
 
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author Dell
  */
 public class Post_Manager {
+  
+   private static final String BASE_FOLDER = "INSTA_RAIZ";
+   private static final String POST_FILE = BASE_FOLDER+"/posts.ins";
+   private RandomAccessFile postsFile;
+   
+   
+    
+    
     
 }
