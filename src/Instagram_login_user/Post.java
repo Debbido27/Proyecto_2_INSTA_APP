691728c30b658c8e46f519057849ffc0fab8dd65
Debbido27 <davidmedina272004@gmail.com>
2026-03-10 21:19:34 -0600

    Se hicieron los guetters y setters

--- src/Instagram_login_user/Post.java
+++ src/Instagram_login_user/Post.java
@@ -19,6 +19,62 @@ public class Post {
       this.tipoMedia=tipoMedia;
       this.fecha=fecha;
   }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getHashtags() {
+        return hashtags;
+    }
+
+    public void setHashtags(String hashtags) {
+        this.hashtags = hashtags;
+    }
+
+    public String getMenciones() {
+        return menciones;
+    }
+
+    public void setMenciones(String menciones) {
+        this.menciones = menciones;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public String getTipoMedia() {
+        return tipoMedia;
+    }
+
+    public void setTipoMedia(String tipoMedia) {
+        this.tipoMedia = tipoMedia;
+    }
+
+    public long getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(long fecha) {
+        this.fecha = fecha;
+    }
   
   
     
